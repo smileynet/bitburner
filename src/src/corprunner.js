6bcfedcaecd8b8aff0fb1a917b73bde40a999325
smileynet <sambiggins@gmail.com>
2022-03-19 00:17:56 -0700

    corp stuff

--- src/src/corprunner.js
+++ src/src/corprunner.js
@@ -2,7 +2,6 @@ export class CorpRunner {
     constructor(ns, messenger) {
         this.messenger = messenger;
         this.corp_api = eval('ns.corporation')
-        this.corp = this.corp_api.getCorporation()
         this.target_warehouse_utilization = 0.7
         this.target_warehouse_level = 10
         this.advert_max = 10
@@ -10,11 +9,18 @@ export class CorpRunner {
     }
 
     async run(ns) {
+        this.handle_research(ns)
+        this.grow_division(ns)
         await this.upgrade_offices(ns)
         this.upgrade_warehouses(ns)
         this.upgrade_advert(ns)
         this.corp_upgrades(ns)
+        this.update_products(ns)
         this.preferred_material_handler(ns)
+    }
+
+    get corp() {
+        return this.corp_api.getCorporation()
     }
 
     get cities() {
@@ -25,18 +31,79 @@ export class CorpRunner {
         return this.corp_api.getCorporation().funds
     }
 
+    materials_produced(industry) {
+        const industries = {
+            software: ['AI Cores'],
+            agriculture: ['Food', 'Plants'],
+            food: [],
+        }
+        return industries[industry.toLowerCase()];
+    }
+
+    handle_research(ns) {
+        for (const division of this.corp.divisions) {
+            if (!this.corp_api.hasResearched(division.name, 'Hi-Tech R&D Laboratory') &&
+                this.corp_api.getResearchCost(division.name, 'Hi-Tech R&D Laboratory') < division.research) {
+                this.corp_api.research(division.name, 'Hi-Tech R&D Laboratory')
+                ns.tprint(`${division.name} has researched Hi-Tech R&D Laboratory`)
+            } else if (division.research > 70000) {
+                this.corp_api.research(division.name, 'Market-TA.I')
+                this.corp_api.research(division.name, 'Market-TA.II')
+                ns.tprint(`${division.name} has researched Market-TA.I & II`)
+            }
+        }
+    }
+
+    update_products(ns) {
+        for (const division of this.corp.divisions) {
+            for (const product of division.products) {
+                if (this.corp_api.hasResearched(division.name, 'Market-TA.II')) {
+                    this.corp_api.setProductMarketTA2(division.name, product, true)
+                } else {
+                    this.corp_api.sellProduct(division.name, 'Sector-12', product, 'MAX', 'MP', true);
+                }
+            }
+            const produced_materials = this.materials_produced(division.type);
+            for (const material of produced_materials) {
+                for (const city of division.cities) {
+                    if (this.corp_api.hasResearched(division.name, 'Market-TA.II')) {
+                        this.corp_api.setMaterialMarketTA2(division.name, city, material, true)
+                    } else {
+                        this.corp_api.sellMaterial(division.name, city, material, 'MAX', 'MP');
+                    }
+                }
+            }
+        }
+    }
+
     preferred_material(industry) {
         const industries = {
             software: `Hardware`,
             agriculture: `Real Estate`,
+            food: `AI Cores`,
         }
         return industries[industry.toLowerCase()];
+    }
+
+    grow_division(ns) {
+        for (const division of this.corp.divisions) {
+            for (const city of this.cities) {
+                if (!division.cities.includes(city) &&
+                    this.corp_api.getExpandCityCost() < this.current_money) {
+                    this.corp_api.expandCity(division.name, city)
+                    ns.tprint(`${division.name} expanded to ${city}.`)
+                }
+            }
+        }
     }
 
     preferred_material_handler(ns) {
         for (const division of this.corp.divisions) {
             const preferred_material = this.preferred_material(division.type);
             for (const city of division.cities) {
+                if (!this.corp_api.hasWarehouse(division.name, city)) {
+                    continue
+                }
                 const warehouse = this.corp_api.getWarehouse(division.name, city);
                 const warehouse_utilization = warehouse.sizeUsed / warehouse.size;
                 if (warehouse_utilization < this.target_warehouse_utilization) {
@@ -44,7 +111,7 @@ export class CorpRunner {
                         ns.tprint(`Buying ${preferred_material} in ${city}`)
                     }
                     this.corp_api.setSmartSupply(division.name, city, false);
-                    this.corp_api.buyMaterial(division.name, city, preferred_material, 400);
+                    this.corp_api.buyMaterial(division.name, city, preferred_material, 50);
                 } else {
                     if (!warehouse.smartSupplyEnabled) {
                         ns.tprint(`Not buying ${preferred_material} in ${city}`)
@@ -53,6 +120,11 @@ export class CorpRunner {
                     this.corp_api.setSmartSupply(division.name, city, true);
                     this.corp_api.setSmartSupplyUseLeftovers(division.name, city, preferred_material, false);
                 }
+                if (warehouse_utilization > 0.9) {
+                    this.corp_api.sellMaterial(division.name, city, preferred_material, 50, 'MP');
+                } else {
+                    this.corp_api.sellMaterial(division.name, city, preferred_material, 0, 0);
+                }
             }
         }
     }
@@ -60,6 +132,14 @@ export class CorpRunner {
     upgrade_warehouses(ns) {
         for (const division of this.corp.divisions) {
             for (const city of division.cities) {
+                if (!this.corp_api.hasWarehouse(division.name, city)) {
+                    if (this.corp_api.getPurchaseWarehouseCost() <= this.current_money) {
+                        this.corp_api.purchaseWarehouse(division.name, city)
+                        ns.tprint(`Warehouse puchased for ${division.name} in ${city}.`)
+                    } else {
+                        continue;
+                    }
+                }
                 const warehouse = this.corp_api.getWarehouse(division.name, city);
                 const warehouse_utilization = warehouse.sizeUsed / warehouse.size;
                 if (warehouse.level <= this.target_warehouse_level &&
