467370c70d2a245570ef046ac98393f20bfd5957
smileynet <sambiggins@gmail.com>
2022-05-31 09:37:01 -0700

    abstracting reserved ram

--- src/utils/boost.js
+++ src/utils/boost.js
@@ -1,8 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+    let reserved_ram = 100
+    if (ns.args[0]) reserved_ram = ns.args[0]
     const script_name = '/utils/share.js'
     let available_ram = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
-    available_ram -= 100
+    available_ram -= reserved_ram
     const ram_cost = ns.getScriptRam(script_name, 'home')
     const max_threads = Math.floor(available_ram / ram_cost)
     ns.tprint(`Spawning ${script_name} with ${max_threads} threads`)
